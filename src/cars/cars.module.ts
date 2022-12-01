08037764ac134d373d3beee2615102536cb15922
Alejandro González Reyes <alexglzrys@gmail.com>
2022-12-01 17:45:07 -0600

    generar módulo cars

---
+++ src/cars/cars.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+
+// Módulo que agrupa todo lo relacionado a los automóviles gestionados por la aplciación
+// * Comando: nest g mo cars
+@Module({})
+export class CarsModule {}
