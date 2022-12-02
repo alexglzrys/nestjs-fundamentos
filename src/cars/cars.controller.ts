d96e43b6e519d04a70c1bc023c5ef62681c77e75
Alejandro González Reyes <alexglzrys@gmail.com>
2022-12-01 18:20:38 -0600

    obtener un auto por su id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 // Controlador para el módulo de automóviles
 // * Comando: nest g co cars
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Jeep'];
   // Ruta para listar todos los autos
   // ? http;//localhost:3000/cars
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Jeep'];
+    return this.cars;
+  }
+
+  // Ruta para devolver un solo auto por su id
+  // ? http;//localhost:3000/cars/param
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    return {
+      id: this.cars[id],
+    };
   }
 }
