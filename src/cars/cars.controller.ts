4cd8813047e77653908438559b472e646f4b6e15
Alejandro González Reyes <alexglzrys@gmail.com>
2022-12-02 16:42:14 -0600

    pipe - ParseUUIDPipe

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   Param,
   ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
@@ -28,8 +29,9 @@ export class CarsController {
   // ? http;//localhost:3000/cars/param
   // El decorador Param contiene los parámetros de ruta enviados en la URL
   // Los Pipes en los parámetros, nos permiten transformar la data del request. Por defecto todo parámetro enviado en la ruta, Nest la recupera como string. Si la data no se puede transformar, automáticamente, NEST lanza un error HTTP personalizado
+  // UUID tiene varias versiones, si deseamos trabajar con una en espécifico, es necesario ajustar el pipe validator -- new ParseUUIDPipe({version: '3'})
   @Get(':id')
-  getCarById(@Param('id') id: string) {
+  getCarById(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
     return this.carsService.findOneById(id);
   }
 
